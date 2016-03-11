357cf605de9a46b0d65812b47569fa51485523c7
Henri Verroken <henriverroken@gmail.com>
2016-03-11 23:44:41 +0100

    Add sys_now syscall that returns the wall-clock time

--- include/syscalls.h
+++ include/syscalls.h
@@ -11,8 +11,14 @@
 extern int const sys_argc;
 extern char *const sys_argv[];
 
+struct sys_now_t {
+	uint64_t sec;
+	uint32_t nsec;
+};
+
 int32_t sys_write(uint64_t fd, void *p, int32_t n);
 uint64_t sys_nanotime(void);
+void sys_now(struct sys_now_t *now);
 uint64_t sys_thread_id(void);
 void sys_usleep(uint32_t usec);
 void sys_nanosleep(uint64_t nsec);
