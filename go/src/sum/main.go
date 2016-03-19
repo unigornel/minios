b9fc3428f9aabc9e2e2503d5b338db917fd68a6a
Henri Verroken <henriverroken@gmail.com>
2016-03-19 22:19:26 +0100

    Put hello world in an endless loop and print the current time

--- go/src/sum/main.go
+++ go/src/sum/main.go
@@ -10,13 +10,8 @@ func main() {}
 
 //export Main
 func Main(unused int) {
-	fmt.Println("Hello World!")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Hello World!")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Hello World!")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Hello World!")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Hello World!")
+	for {
+		fmt.Printf("[%v] Hello World!\n", time.Now().UTC())
+		time.Sleep(1 * time.Second)
+	}
 }
